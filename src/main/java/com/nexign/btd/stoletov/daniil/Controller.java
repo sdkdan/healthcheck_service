95c6883a6094565bfcf9bcc540d1b75a39f4c4f6
sdkdan <49169838+sdkdan@users.noreply.github.com>
2020-04-29 19:47:12 +0300

    Update Controller.java
    test

--- src/main/java/com/nexign/btd/stoletov/daniil/Controller.java
+++ src/main/java/com/nexign/btd/stoletov/daniil/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-//test
+//test11
     @RequestMapping(value="health/check", method = RequestMethod.GET)
     @ResponseBody
     public String check(@RequestParam(value="answerTo", required=false)
